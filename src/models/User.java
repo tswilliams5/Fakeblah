56c2d52b17c3a0f428c1dff9987db257583e6520
bill jones <bill.jones@eysys.com>
2015-03-10 20:55:50 +0000

    beginning of user class

--- src/models/User.java
+++ src/models/User.java
@@ -1,7 +1,18 @@
 package models;
 
+import java.util.Date;
+
 /**
  * Created by billjones on 10/03/15.
  */
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String passwordHash;
+    private Date dateJoined;
+    private Date lastSeen;
+    private Date birthday;
+    
 }
